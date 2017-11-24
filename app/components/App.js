45c437e37530da85e4cca413caf36786b5eb3d4e
Lyle Innes <lyle.innes@gmail.com>
2017-11-24 12:56:32 +1100

    reworking starterpack

---
+++ app/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return <div>
+      <h1>hello world</h1>
+    </div>
+  }
+
+}
+
+module.exports = App
