303c34b87dadc466128c51f0376a2eb943df2208
Lyle Innes <lyle.innes@gmail.com>
2017-11-27 13:59:51 +1100

    about to convert tileState to array

--- app/components/Game.js
+++ app/components/Game.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import _ from 'underscore'
 // import Tile from './Tile'
 
 
@@ -7,13 +8,17 @@ export default class Game extends React.Component {
     super(props)
     this.createTile = this.createTile.bind(this)
     this.findSurroundingTiles = this.findSurroundingTiles.bind(this)
+    this.swapTiles = this.swapTiles.bind(this)
+    // this.componentWillMount = this.componentWillMount.bind(this)
+    this.checkUnderscore = this.checkUnderscore.bind(this)
     this.state = {
+      win: false,
       tiles: [
       {
         displayed: '1'  
       },
       {
-        displayed: '2'  
+        displayed: '2'
       },
       {
         displayed: '3'  
@@ -39,26 +44,48 @@ export default class Game extends React.Component {
     }
   }
 
-  findSurroundingTiles(tileObj, index, tileArray) {
-    if (index - 3 >= 0) {
-      var above = index - 3;
-      console.log(this.state.tiles[above])
+  swapTiles(aboveInd, clickedInd) {
+    var tiles = this.state.tiles;
+    var clickedTileVal = tiles[clickedInd].displayed;
+    var aboveTileVal = tiles[aboveInd].displayed;
+    tiles[clickedInd].displayed = aboveTileVal;
+    tiles[aboveInd].displayed = clickedTileVal
+    this.setState({
+      tiles: tiles,
+    })
+  }
+
+  findSurroundingTiles(clickedIndex, tileArray) {
+    if (clickedIndex - 3 >= 0) {
+      var aboveInd = clickedIndex - 3;
+      if (tileArray[aboveInd].displayed == "") {
+        this.swapTiles(aboveInd, clickedIndex)
+      }
     }
-    if (index + 3 <= 8) {
-      var below = index + 3;
-      console.log(this.state.tiles[below])
+    if (clickedIndex + 3 <= 8) {
+      var belowInd = clickedIndex + 3;
+      if (tileArray[belowInd].displayed == "") {
+        this.swapTiles(belowInd, clickedIndex)
+      }
     }
-    if (index == 1 || index == 4 || index == 7) {
-      var left = index - 1;
-      var right = index + 1;
-      console.log(this.state.tiles[right])
-      console.log(this.state.tiles[left])
-    } else if (index == 0 || index == 3 || index == 6) {
-      var right = index + 1;
-      console.log(this.state.tiles[right])
-    } else if (index == 2 || index == 5 || index == 8) {
-      var left = index - 1;
-      console.log(this.state.tiles[left])
+    if (clickedIndex == 1 || clickedIndex == 4 || clickedIndex == 7) {
+      var leftInd = clickedIndex - 1;
+      var rightInd = clickedIndex + 1;
+      if (tileArray[leftInd].displayed == "") {
+        this.swapTiles(leftInd, clickedIndex)
+      } else if (tileArray[rightInd].displayed == "") {
+        this.swapTiles(rightInd, clickedIndex)
+      }
+    } else if (clickedIndex == 0 || clickedIndex == 3 || clickedIndex == 6) {
+      var rightInd = clickedIndex + 1;
+      if (tileArray[rightInd].displayed == "") {
+        this.swapTiles(rightInd, clickedIndex)
+      }
+    } else if (clickedIndex == 2 || clickedIndex == 5 || clickedIndex == 8) {
+      var leftInd = clickedIndex - 1;
+      if (tileArray[leftInd].displayed == "") {
+        this.swapTiles(leftInd, clickedIndex)
+      }
     }
   }
 
@@ -66,15 +93,28 @@ export default class Game extends React.Component {
     return <div
     className="tile"
     key={index}
-    onClick={() => this.findSurroundingTiles(tileObj, index, tileArray)}>
+    onClick={() => this.findSurroundingTiles(index, tileArray)}>
       <p>{tileObj.displayed}</p>
     </div>
   }
 
+  checkUnderscore(array) {
+    return _.shuffle(array)
+  }
+
+  // componentWillMount() {
+  //   var tiles = this.state.tiles
+  //   var tiles.map
+  // }
+
   render() {
+    let testArray = [1,2,3,4,5,6];
     return (
       <div className="board">
          {this.state.tiles.map(this.createTile)}
+        <div>
+          <p>{this.checkUnderscore(testArray)}</p>
+        </div>
       </div>
     )
   }
