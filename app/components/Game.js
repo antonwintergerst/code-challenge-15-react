a4cd806037910b63ab719b9271f4558d97c73d6d
Lyle Innes <lyle.innes@gmail.com>
2017-11-27 14:25:58 +1100

    cleaning out unnecessary code

--- app/components/Game.js
+++ app/components/Game.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import _ from 'underscore'
-// import Tile from './Tile'
 
 
 export default class Game extends React.Component {
@@ -9,81 +8,54 @@ export default class Game extends React.Component {
     this.createTile = this.createTile.bind(this)
     this.findSurroundingTiles = this.findSurroundingTiles.bind(this)
     this.swapTiles = this.swapTiles.bind(this)
-    // this.componentWillMount = this.componentWillMount.bind(this)
     this.checkUnderscore = this.checkUnderscore.bind(this)
     this.state = {
       win: false,
-      tiles: [
-      {
-        displayed: '1'  
-      },
-      {
-        displayed: '2'
-      },
-      {
-        displayed: '3'  
-      },
-      {
-        displayed: '4'  
-      },
-      {
-        displayed: '5'  
-      },
-      {
-        displayed: '6'  
-      },
-      {
-        displayed: '7'  
-      },
-      {
-        displayed: '8'  
-      },
-      {
-        displayed: ''  
-      }]
+      tiles: ['1','2','','4','5','6','7','8','3']
     }
   }
 
-  swapTiles(aboveInd, clickedInd) {
+  swapTiles(destinationInd, clickedInd) {
     var tiles = this.state.tiles;
-    var clickedTileVal = tiles[clickedInd].displayed;
-    var aboveTileVal = tiles[aboveInd].displayed;
-    tiles[clickedInd].displayed = aboveTileVal;
-    tiles[aboveInd].displayed = clickedTileVal
+    var clickedTileVal = tiles[clickedInd];
+    var destinationTileVal = tiles[destinationInd];
+    tiles[clickedInd] = destinationTileVal;
+    tiles[destinationInd] = clickedTileVal
     this.setState({
       tiles: tiles,
     })
   }
 
   findSurroundingTiles(clickedIndex, tileArray) {
+    console.log(tileArray)
     if (clickedIndex - 3 >= 0) {
       var aboveInd = clickedIndex - 3;
-      if (tileArray[aboveInd].displayed == "") {
+      if (tileArray[aboveInd] == '') {
         this.swapTiles(aboveInd, clickedIndex)
       }
     }
     if (clickedIndex + 3 <= 8) {
       var belowInd = clickedIndex + 3;
-      if (tileArray[belowInd].displayed == "") {
+      if (tileArray[belowInd] == '') {
         this.swapTiles(belowInd, clickedIndex)
       }
     }
     if (clickedIndex == 1 || clickedIndex == 4 || clickedIndex == 7) {
       var leftInd = clickedIndex - 1;
       var rightInd = clickedIndex + 1;
-      if (tileArray[leftInd].displayed == "") {
+      if (tileArray[leftInd] == '') {
         this.swapTiles(leftInd, clickedIndex)
-      } else if (tileArray[rightInd].displayed == "") {
+      } else if (tileArray[rightInd] == '') {
         this.swapTiles(rightInd, clickedIndex)
       }
     } else if (clickedIndex == 0 || clickedIndex == 3 || clickedIndex == 6) {
       var rightInd = clickedIndex + 1;
-      if (tileArray[rightInd].displayed == "") {
+      if (tileArray[rightInd] == '') {
         this.swapTiles(rightInd, clickedIndex)
       }
     } else if (clickedIndex == 2 || clickedIndex == 5 || clickedIndex == 8) {
       var leftInd = clickedIndex - 1;
-      if (tileArray[leftInd].displayed == "") {
+      if (tileArray[leftInd] == '') {
         this.swapTiles(leftInd, clickedIndex)
       }
     }
@@ -94,7 +66,7 @@ export default class Game extends React.Component {
     className="tile"
     key={index}
     onClick={() => this.findSurroundingTiles(index, tileArray)}>
-      <p>{tileObj.displayed}</p>
+      <p>{tileObj}</p>
     </div>
   }
 
@@ -102,19 +74,18 @@ export default class Game extends React.Component {
     return _.shuffle(array)
   }
 
-  // componentWillMount() {
-  //   var tiles = this.state.tiles
-  //   var tiles.map
-  // }
+  componentWillMount() {
+    var tiles = this.state.tiles
+    tiles = _.shuffle(tiles)
+    this.setState({
+      tiles: tiles
+    })
+  }
 
   render() {
-    let testArray = [1,2,3,4,5,6];
     return (
       <div className="board">
          {this.state.tiles.map(this.createTile)}
-        <div>
-          <p>{this.checkUnderscore(testArray)}</p>
-        </div>
       </div>
     )
   }
